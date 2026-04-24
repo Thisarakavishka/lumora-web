b7c1767d927bb11aa2ee9e01195886e95a41cfe0
Thisara kavishka <thisarakavishka0@gmail.com>
2026-04-25 00:25:12 +0530

    Feat: Add new Luxury section for Luxury look for landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Hero from "../app/components/sections/Hero";
 import Offers from "../app/components/sections/Offers";
 import Treatments from "../app/components/sections/Treatments";
+import Experience from "./components/sections/Experience";
+import LuxuryIntro from "./components/sections/LuxuryIntro";
 
 export default function Home() {
   return (
     <div className="flex flex-col w-full">
       <Hero />
+      <LuxuryIntro />
+      <Experience />
       <Offers />
       <Treatments />
     </div>
