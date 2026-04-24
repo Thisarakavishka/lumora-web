7eb4cff636d267153a0c222738634658637f1554
Thisara kavishka <thisarakavishka0@gmail.com>
2026-04-25 01:00:32 +0530

    Fix: Fix bug on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "../app/components/Navbar";
-import Footer from "../app/components/Footer";
+// import Footer from "../app/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,7 +35,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col bg-zinc-900 text-zinc-50">
         <Navbar />
         <main className="flex-grow">{children}</main>
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
